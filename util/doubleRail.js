69bc1b4a428188acf0397ed0e91e19bd0d2150b2
mk-pmb <mk-pmb@users.noreply.github.com>
2020-07-30 02:41:08 +0200

    v0.2.5: doubleRail: Smarter quotes welding

--- util/doubleRail.js
+++ util/doubleRail.js
@@ -14,25 +14,40 @@ function railAdd(active, tx) {
   return tx;
 }
 
+
+function joinStr(parts) { return parts.join(this); }
+
+function unquoteWeld(parts) {
+  return parts.reduce(function add(have, next) {
+    if (have.slice(-1) === next.slice(0, 1)) {
+      return have.slice(0, -1).concat(next.slice(1));
+    }
+    return have.concat(next);
+  });
+}
+
+
 function doubleRail(diff, opt) {
   if (!opt) { opt = false; }
-  var hunkSep = (opt.hunkSep || ' […] '),
-    quot = (opt.quot || JSON.stringify),
-    oldRail = '--- ',
-    newRail = '+++ ';
+  var oldRail = [], newRail = [], weld = opt.weld,
+    hunkSep = (opt.hunkSep || ' […] '),
+    quot = (opt.quot || JSON.stringify);
   diff.forEach(function railHunk(hunkData, hunkIdx) {
     if (hunkIdx) {
-      oldRail += hunkSep;
-      newRail += hunkSep;
+      oldRail.push(hunkSep);
+      newRail.push(hunkSep);
     }
     hunkData.forEach(function (part) {
       var sign = part[0], text = quot(part[1]);
-      oldRail += railAdd((sign !== '+'), text);
-      newRail += railAdd((sign !== '-'), text);
+      oldRail.push(railAdd((sign !== '+'), text));
+      newRail.push(railAdd((sign !== '-'), text));
     });
   });
-  oldRail += (opt.oldRailAnnot || '');
-  newRail += (opt.newRailAnnot || '');
+  if (weld === false) { return { o: oldRail, n: newRail, d: diff }; }
+  if (typeof weld === 'string') { return joinStr.bind(weld); }
+  if (!weld) { weld = unquoteWeld; }
+  oldRail = '--- ' + weld(oldRail, opt) + (opt.oldRailAnnot || '');
+  newRail = '+++ ' + weld(newRail, opt) + (opt.newRailAnnot || '');
   return [oldRail, newRail, String(diff)].join('\n');
 }
 
